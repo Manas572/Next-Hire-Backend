05c057987aa699801f54091c28de91ce7c9a1311
Manas Kumar <manaskumar200704@gmail.com>
2026-07-02 02:30:53 +0530

    urls added

--- NextHireBackend/urls.py
+++ NextHireBackend/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from CreateUser.views import reg_candidate,reg_recruiter,education_register,project_register,experience_register,CandidateProfileView
+from CreateUser.views import reg_candidate,reg_recruiter,education_register,project_register,experience_register,CandidateProfileView,EducationListView,ExperienceListView,ProjectListView,ProjectUpd,ExperienceUpd,EducationUpd
 from jobposting.views import CreateJob,ListAlljob,ApplyJob,JobDetail
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -13,10 +13,16 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("createcandiate/",reg_candidate.as_view(),name="Register_Candidate"),
     path("me/",CandidateProfileView.as_view(),name="Candidate_profile"),
+    path("edu/",EducationListView.as_view(),name="education_info"),
+    path("exp/",ExperienceListView.as_view(),name="experience_info"),
+    path("pro/",ProjectListView.as_view(),name="project_info"),
+    path("eduupd/",EducationUpd.as_view(),name="education_update"),
+    path("expupd/",ExperienceUpd.as_view(),name="experience_update"),
+    path("proupd/",ProjectUpd.as_view(),name="project_update"),
     path("createrecruiter/",reg_recruiter.as_view(),name="Register_recruiter"),
-    path("education/",education_register.as_view(),name="education_recruiter"),
-    path("experience/",experience_register.as_view(),name="experience_recruiter"),
-    path("project/",project_register.as_view(),name="project_recruiter"),
+    path("education/",education_register.as_view(),name="education_register"),
+    path("experience/",experience_register.as_view(),name="experience_register"),
+    path("project/",project_register.as_view(),name="project_register"),
     path("createjob/",CreateJob.as_view(),name="Create_job"),
     path("listjob/",ListAlljob.as_view(),name="List_job"),
     path("listjob/<int:pk>/", JobDetail.as_view(), name="job-detail"),
